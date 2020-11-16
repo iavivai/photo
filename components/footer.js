e5dccac48e2446ff7d87fb7af4e4bfedd28d66a7
iavivai <hello@yukitakaiha.com>
2020-11-16 23:37:59 +0900

    Fixed to use constant.

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import { NAME } from '../lib/constants'
 
 export default function Footer() {
   return (
     <footer className={utilStyles.footer}>
       <div className={utilStyles.copyright}>
-        © 2019 - 2020 iavivai, All rights reserved
+        © {NAME}, All rights reserved
       </div>
     </footer>
   )
